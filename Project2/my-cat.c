ffd0c4543e6b03f79fdcda2931d3b65f08b7984c
LatePate33 <79865206+LatePate33@users.noreply.github.com>
2022-04-09 22:07:15 +0300

    Update my-cat.c

--- Project2/my-cat.c
+++ Project2/my-cat.c
@@ -5,20 +5,22 @@ int main(int argc, char *argv[]){
   char str[150];
   int i = 1;
   
+  // Check if no command-line arguments are passed
   while(argc != 1)
   {
     FILE *fp = fopen(argv[i], "r");
+    // Check the file is openable
     if(fp == NULL)
     {
       printf("wcat: cannot open file\n");
       exit(1);
     }
-    while(fgets(str, 150, fp))
+    while(fgets(str, 150, fp)) // https://man7.org/linux/man-pages/man3/fgets.3p.html
     {
       printf("%s", str);
     }
-    --argc;
-    ++i;
+    --argc; // -1 from the argc, if multiple files
+    ++i; // +1 to open the next file
     fclose(fp);
   }
   
