bcc70925e403f4e47a5f611e083ce98311966104
LatePate33 <79865206+LatePate33@users.noreply.github.com>
2022-04-22 13:41:07 +0300

    Update my-zip.c

--- Project2/my-zip.c
+++ Project2/my-zip.c
@@ -2,69 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 
-void argument_length(int arg)
-{
-  if(arg <= 1)
-  {
+void encode(int count, int compare) {
+  char str[2];
+  str[0] = compare;
+  fwrite(&count, 4, 1, stdout);
+  fwrite(&str, 1, 1, stdout);
+}
+
+int main(int argc, char *argv[]) {
+  int i;
+  char buffer[2];
+  int compare;
+  int count = 0;
+  
+  if (argc <= 1) {
     printf("wzip: file1 [file2 ...]\n");
     exit(1);
   }
-
-}
-
-void encode(char *str)
-{
-  int count;
-  for(int j = 0; str[j]; j++)
-  {
-    if(str[j+1] != '\0')
-    {
-      count = 1;
-      while(str[j] == str[j+1])
-      {
-        count++, j++;
+  else {
+    for (i = 1; i < argc; i++) {
+      FILE *fp = fopen (argv[i], "r");
+      if (fp == NULL) {
+        printf("wzip: cannot open file\n");
+        exit(1);
       }
-      fwrite(&count, sizeof(int), 1, stdout);
-      fwrite(&str[j], 1, 1, stdout);
+      while(fread(buffer, 1, 1, fp)) {
+        if (compare == 3) {
+          break;
+        }
+        if (buffer[0] == compare) {
+          count++;
+        }
+        else {
+          if (count != 0) {
+            encode(count, compare);
+          }
+          count = 1;
+          compare = buffer[0];
+        }
+        
+      }
+      fclose(fp);
     }
+  
   }
-free(str);
-}
-
-int main(int argc, char *argv[])
-{
-  argument_length(argc);
-  int i = 1;
-  
-  char buffer[2];
-  
-  while(argc != 1)
-  {
-    int count2 = 0;
-    FILE *fp = fopen(argv[i], "rb");
-    if(fp == NULL)
-    {
-      printf("wzip: cannot open file\n");
-      exit(1);
-    }
-    //Count characters
-    while(fread(buffer,1,1, fp) == 1)
-    {
-      ++count2;
-    }
-    char *str = malloc(sizeof(char) * count2);
-    rewind(fp);
-    //Read characters into to a string
-    while(fread(buffer, 1, 1, fp) == 1)
-    {
-      strncat(str, buffer, 1);
-    }
-    printf("%s\n", str);
-    encode(str);
-    --argc;
-    ++i;
-    fclose(fp);
-  }
-  
   return 0;
 }
